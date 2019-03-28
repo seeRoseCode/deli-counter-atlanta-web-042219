24bf49b128e36d1d1d4880b6e1bc03f3c737ff62
Kesha C Lake <keshalakemusic@gmail.com>
2019-03-28 02:42:16 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,3 +19,13 @@ def take_a_number(array, name)
   number += 1
   puts "Welcome, #{name}. You are number #{number} in line."
 end
+
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    current_person = array.first
+    puts "Currently serving #{current_person}."
+    array.shift
+  end
+end
