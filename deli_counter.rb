7f581c185f7d8abe8bb87122cf2b1c2ce14dba27
Kesha C Lake <keshalakemusic@gmail.com>
2019-03-28 00:19:57 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    line_list = ""
+    array.each.with_index(1) do |name, index|
+      person = " #{index}. #{name}"
+      line_list << person
+    end
+    puts "The line is currently:#{line_list}"
+  end
+end
